8569d0504a9a8f272f726f15cd8bbce1a8d2ffb3
JUNMET <1611859097@qq.com>
2020-01-28 19:06:48 +0800

    商品列表、实现搜索框和tabs组件

---
+++ components/Tabs/Tabs.js
@@ -0,0 +1,32 @@
+// components/Tabs/Tabs.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    tabs: {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 点击事件
+    handleItemTap(e) {
+      // 获取点击的索引值
+      const {index} = e.currentTarget.dataset;
+      // 触发 父元组件的事件  子传父 myevent是自定义事件名称
+      this.triggerEvent("myevent",{index})
+    }
+  }
+})
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -5,9 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs:[
+      {
+        id:0,
+        value:"综合",
+        isActive:true
+      },
+      {
+        id:1,
+        value:"销量",
+        isActive:false
+      },
+      {
+        id:2,
+        value:"价格",
+        isActive:false
+      }
+    ]
   },
 
+  onMyevent(e){
+    // console.log(e);
+    // 获取点击索引值  是从子组件传递过来的值
+    const {index} = e.detail
+    // 修改tabs数组
+    let {tabs} = this.data
+    tabs.forEach((v,i)=>{    
+      if(i===index){
+        v.isActive=true
+      }else{
+        v.isActive=false
+      }
+    })
+    this.setData({
+      tabs
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -41,27 +74,6 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
