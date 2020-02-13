d8121ef7312804056a2093cb7cf8b1d773cba9ac
JUNMET <1611859097@qq.com>
2020-02-13 18:50:20 +0800

    订单查询页面引入tabs自定义组件

--- pages/order/index.js
+++ pages/order/index.js
@@ -5,14 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs:[
+      {
+        id:0,
+        value:"全部",
+        isActive: true
+      },
+      {
+        id:1,
+        value:"待付款",
+        isActive: false
+      },
+      {
+        id:2,
+        value:"待收货",
+        isActive: false
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options);
+    
   },
 
   /**
@@ -29,38 +46,10 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onMyevent(e){
+    const {index} = e.detail
+    const {tabs} = this.data
+    tabs.forEach((v,i) => i===index?v.isActive=true:v.isActive=false);
+    this.setData({tabs})
   }
 })
