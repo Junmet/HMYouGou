c861579578ba561979bb2a5458ccff120eaff43b
JUNMET <1611859097@qq.com>
2020-02-14 17:23:40 +0800

    根据type值激活不同标题并发送请求

--- pages/order/index.js
+++ pages/order/index.js
@@ -24,22 +24,26 @@ Page({
       }
     ],
     // 订单数据
-    orders:[]
+    orders: []
   },
 
 
   onShow: function () {
     const token = wx.getStorageSync("token");
     // 没有token值 跳转授权页面
-    if(!token) {
-      wx.navigateTo({url: '/pages/auth/index',});
+    if (!token) {
+      wx.navigateTo({url: '/pages/auth/index'});
       return;
     }
-    
+
     // 获取当前页面栈。数组中第一个元素为首页，最后一个元素为当前页面。
     let page = getCurrentPages()
     // 获取页面参数
-    const type = page[page.length - 1].options
+    const {type} = page[page.length - 1].options
+    
+    // 根据type选中激活标题
+    this.titleIndex(type-1)
+
     // 调用获取订单数据
     this.getOrder(type)
   },
@@ -48,31 +52,31 @@ Page({
   async getOrder(type) {
     // 获取token值
     const token = wx.getStorageSync("token");
+
     // 请求头
     const header = {Authorization: token}
+
     // 发送求
-    const res = await request({
-      url: "/my/orders/all",
-      header:header,
-      data:type
-    })
+    const res = await request({url: "/my/orders/all",header: header,data: {type}})
     const {orders} = res.data.message
     this.setData({orders})
   },
 
   onMyevent(e) {
     // 获取被点击的索引值
-    const {
-      index
-    } = e.detail
+    const {index} = e.detail
+    
+    this.titleIndex(index)
+    // 估计不一样的type请求不同的数据 index+1 === type
+    this.getOrder(index+1)
+  },
+
+  // 根据标题的索引 来激活选中的标题
+  titleIndex(index){
     // 修改原数组
-    const {
-      tabs
-    } = this.data
+    const {tabs} = this.data
     // 遍历数组
     tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
-    this.setData({
-      tabs
-    })
+    this.setData({tabs})
   }
 })
