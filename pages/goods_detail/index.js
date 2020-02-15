e63311a1d2ca7ae93d2eb4f48b27131124e782a9
JUNMET <1611859097@qq.com>
2020-02-15 16:14:16 +0800

    商品收藏功能完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,11 +1,14 @@
 import {request} from "../../request/request.js"
+import {showToast} from "../../utils/asyncWx.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    commodityDetails: {}
+    commodityDetails: {},
+    // 商品是否被收藏 默认没有
+    isCollect:false
   },
 
   // 商品详情返回全部的总数据
@@ -14,10 +17,9 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: async function (options) {
-    const {
-      goods_id
-    } = options
+  onShow: async function () {
+    let page = getCurrentPages()
+    const {goods_id} = page[page.length - 1].options
     this.commodity(goods_id)
   },
 
@@ -25,12 +27,17 @@ Page({
   async commodity(goods_id) {
     const res = await request({
       url: '/goods/detail',
-      data: {
-        goods_id
-      },
+      data: {goods_id},
       method:"GET"
     })
     this.allData = res.data.message
+
+    // 商品收藏 获取本地缓存 看是否存在数组中
+    let collect = wx.getStorageSync("collect") || [];
+    // 判断当前商品是否被收藏 以变量的形式判断 true为有 false为没有
+    let isCollect = collect.some(v=>v.goods_id===this.allData.goods_id)
+
+
     this.setData({
       commodityDetails: {
         pics: res.data.message.pics,
@@ -39,7 +46,8 @@ Page({
         // iphone部分手机 不识别 webp图片格式
         // 临时自己修改 要确保后台有 1.webp => 1.jpg
         goods_introduce: res.data.message.goods_introduce.replace(/\.webp/g, ".jpg"),
-      }
+      },
+      isCollect
     })
   },
 
@@ -84,5 +92,32 @@ Page({
       // true 是为了防止用户疯狂点击 设置为true点击一次5秒后 用户才可以再一次点击
       mask: true
     })
+  },
+
+  // 商品收藏
+  async commodityCollect(){
+    //  1.判断该商品是否存在于缓存中
+    //  2.存在就删除
+    //  3.不存在就添加   
+    let isCollect = false
+    // 获取缓存中的商品收藏数组
+    let collect = wx.getStorageSync("collect") || [];
+    // 判断商品是否被收藏果
+     let index = collect.findIndex(v => v.goods_id===this.allData.goods_id)
+     if(index!=-1){
+      //  收藏过 删除该商品
+      collect.splice(index,1)
+      isCollect = false
+      await showToast({title:"取消收藏"})
+     }else{
+      //  没有收藏过
+      collect.push(this.allData)
+      isCollect = true
+      await showToast({title:"收藏成功"})
+     }
+    //  再把数组本地缓存
+     wx.setStorageSync("collect", collect);
+    //  再修改 data 中属性 isCollect的值
+    this.setData({isCollect})
   }
 })
