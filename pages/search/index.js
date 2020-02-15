30648e56a28207ac85beed714323c92a41aafc3b
JUNMET <1611859097@qq.com>
2020-02-15 18:34:49 +0800

    判断搜索的值是否为空、空为不符合给予提示

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,4 +1,5 @@
 import {request} from "../../request/request.js"
+import {showToast} from "../../utils/asyncWx.js"
 Page({
   data: {
     // 搜索列表数据
@@ -7,7 +8,7 @@ Page({
   // 输入框输入的值
   values:"",
   // 输入框的值改变 就会触发这个事件
-  searchCommodity(e){
+   searchCommodity(e){
     const {value} = e.detail
     this.values = value
   },
@@ -15,6 +16,10 @@ Page({
   // 点击搜索按钮时 触发
   async searchBtn(){
     let query = this.values
+    if(!query.trim()){
+      await showToast({title:"输入不合法"})
+      return
+     }
     const res = await request({url:"/goods/search",method:"GET",data:{query}})
     const goods = res.data.message.goods
     this.setData({goods})
