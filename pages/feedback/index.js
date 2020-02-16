adeda99151906a3229f94bcc444c0f75e85af50e
JUNMET <1611859097@qq.com>
2020-02-16 12:13:59 +0800

    意见反馈选择图片并显示图片

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,3 +1,19 @@
 Page({
-
+    data:{
+        // 被选中图片的路径数组
+        chooseImg:[]
+    },
+    addImg(){
+        // 调用小程序内置的选择图片api
+        wx.chooseImage({
+            count: 9,
+            sizeType: ['original','compressed'],
+            sourceType: ['album','camera'],
+            success: (result)=>{
+                   this.setData({
+                       chooseImg:[...this.data.chooseImg,...result.tempFilePaths]
+                    })
+            }
+        });
+    }
 })
